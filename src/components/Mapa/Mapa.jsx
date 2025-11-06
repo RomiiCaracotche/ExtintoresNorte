e1c84e138211062be278d09202a7d3c6e98e4b30
Romii <rocaracotche@gmail.com>
2025-11-06 17:52:29 -0300

    marker

--- src/components/Mapa/Mapa.jsx
+++ src/components/Mapa/Mapa.jsx
@@ -1,12 +1,16 @@
 import './Mapa.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Icon } from "leaflet" ;  
 
-export default function Mapa() {
+export default function Mapa() {    
+
+	const skater = new Icon ({ iconUrl : "/img/marker.png" , iconSize : [ 60 , 60 ] }); 
+
 	return (
 		<MapContainer center={{lat:"-34.8954285", lng:"-57.9912678"}} zoom={17}>
 			<TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' attribution='&copy; <a href=https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
-			<Marker position={{lat:"-34.8954285", lng:"-57.9912678"}}>
+			<Marker position={{lat:"-34.8954285", lng:"-57.9912678"}} icon={skater} >
 				<Popup>
 					Extintores Norte
 				</Popup>
