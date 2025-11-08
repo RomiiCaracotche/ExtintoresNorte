adf204c74d54a056f68569e95918d4600694df0b
Romii <rocaracotche@gmail.com>
2025-11-07 23:19:19 -0300

    mobile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,41 +13,38 @@ export default function Home() {
 
         <div className="container-home">
 
-            <section className="seccion-home">
-                <div>
-                    <img src="/img/animacion/matafuego-bienvenido.png" className="img-bienvenido"/>
-                </div>
+            <img src="/img/animacion/matafuego-bienvenido.png" className="img-bienvenido"/>
+
+            {/* ----------- Seccion home ----------- */}
+            <section className="mt-5">
                 <Container-fluid>
-                    <Row className="row-center-center">
-                        <Col xs={10} sm={8} md={6} lg={5} xl={4} className="col-card">
-                            <Card className="home-card card-impar">
+                    <Row className="row-center-center" >
+                        <Col xs={10} sm={9} md={5} className="card-paridad card-impar">
+                            <Card className="home-card">
                                 <Card.Img variant="top" src="/img/frente.jpg" />
                                 <Card.Body className="home-card-body">
                                     <Card.Text>VENTA Y RECARGA DE MATAFUEGOS</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col xs={10} sm={8} md={6} lg={5} xl={4} className="col-card">
-                            <Card className="home-card card-par">
+                        <Col xs={10} sm={9} md={5} className="card-paridad card-par">
+                            <Card className="home-card">
                                 <Card.Img variant="top" src="/img/local.jpg" className="object-fit-cover"/>
                                 <Card.Body className="home-card-body">
                                     <Card.Text>ELEMENTOS DE PREVENCION Y SEGURIDAD</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
-                    </Row>
-
-                    <Row className="row-center-center">
-                        <Col xs={10} sm={8} md={6} lg={5} xl={4} className="col-card">
-                            <Card className="home-card card-impar">
+                        <Col xs={10} sm={9} md={5} className="card-paridad  card-impar">
+                             <Card className="home-card">
                                 <Card.Img variant="top" src="/img/servicios/camioneta.png" className="object-fit-contain" />
                                 <Card.Body className="home-card-body">
                                     <Card.Text>RETIRO Y ENTREGA A DOMICILIO</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col xs={10} sm={8} md={6} lg={5} xl={4} className="col-card">
-                            <Card className="home-card card-par">
+                        <Col xs={10} sm={9} md={5} className="card-paridad card-par">
+                            <Card className="home-card">
                                 <Card.Img variant="top" src="/img/prevencion.png"/>
                                 <Card.Body className="home-card-body">
                                     <Card.Text>CONOCIMIENTO Y EXPERIENCIA EN PREVENCION DE INCENDIOS</Card.Text>
@@ -58,50 +55,73 @@ export default function Home() {
                 </Container-fluid>
             </section>
 
-            <section className="seccion-horario">
+            {/* ----------- Seccion ubicacion ----------- */}
+            <section className="mt-5">
                 <Container-fluid>
                         <Row className="justify-content-around align-items-center">
-                            <Col xs={12} sm={12} md={8} lg={7} xl={7} className="">
+                            <Col xs={12} lg={6} > 
                                 <Row className="row-center-center">
-                                    <Col xs={12} sm={11} md={3} lg={3} xl={4} className="col-ubicacion">
-                                        <img src="/img/animacion/matafuego-mapa.png" className="img-extintor" />
-                                    </Col>
-                                    <Col xs={12} sm={11} md={9} lg={7} xl={7}>
+                                    <Col xs={12} lg={10} xl={12} >
                                         <div className="column-center-center">
-                                            <h3 className="fw-bold mb-3 mt-5">¿DONDE ESTAMOS?</h3>
-                                            <ul className="li-horario">
-                                                <li>La Plata (1900)</li>
+                                            <h3 className="fw-bold mb-3 mt-5 text-center text-white">PODES ENCONTRARNOS EN</h3>
+                                            <ul className="ul-ubicacion">
+                                                <li>LA PLATA</li>
                                                 <li><FaMapLocationDot /> 519 N° 1820 esquina 13</li>
-                                                <li><MdAlternateEmail /> <a href="mailto:extintoresnorte@hotmail.com">extintoresnorte@hotmail.com</a></li>
-                                                <li><FaPhone /> <a href="tel:02214710750">0221 471-0750</a></li>
-                                                <li><IoLogoWhatsapp /> <a href="https://wa.me/2215448911">221-544-8911</a></li>
-                                                <li><IoLogoWhatsapp /> <a href="https://wa.me/2215369997">221-536-9997</a></li>
-                                                <li><BsInstagram className=""/> <a href="https://www.instagram.com/extintores_norte/">Extintores_norte</a></li>
-                                                <li><FaFacebook /> <a href="https://www.facebook.com/extintoresnorte/?locale=es_LA">Extintores Norte</a></li>
                                             </ul>
                                         </div>
+                                    </Col> 
+                                    <Col xs={12} sm={11} md={12} className="row-center-center">
+                                        <img src="/img/animacion/matafuego-mapa.png" className="img-extintor" />
                                     </Col>
-                                </Row> 
+                                </Row>
                             </Col>
-                            <Col xs={12} sm={12} md={12} lg={5} xl={4} className="p-0 col-mapa" >
+                            <Col xs={12} sm={11} md={9} lg={6} className="col-mapa">
                                 <Mapa />
-                            </Col>  
-                        </Row>
-
-                        <Row>
-                            <Col className="col-horario">
-                                <div className="d-flex flex-column justify-content-around align-items-center mt-5">
-                                    <h3 className="fw-bold mb-3">HORARIOS</h3>
-                                    <ul className="li-horario">
-                                        <li><FaClock /> LUNES a VIERNES de 8.30hs a 17hs</li>
-                                        <li><FaClock /> SABADOS de 8.30hs a 13hs</li>
-                                    </ul>
-                                </div>
-                                <img src="/img/animacion/matafuego-reloj.png" className="img-extintor"/>
                             </Col>
                         </Row>
                 </Container-fluid>
             </section>
+
+            {/* ----------- Seccion contacto/horario ----------- */}
+            <section className="w-100 mt-5">   
+                <Container-fluid>
+                        <Row className="row-contacto_horario">             
+                                <Col xs={12} sm={12} md={8} lg={7} xl={4} className="bg-warning">
+                                    <div className="column-center-center">
+                                        <h3 className="fw-bold mb-3 mt-5 text-center text-white">CONTACTANOS POR</h3>
+                                        <ul className="ul-contacto">
+                                            <li><a href="mailto:extintoresnorte@hotmail.com"><MdAlternateEmail/> extintoresnorte@hotmail.com</a></li>
+                                            <li><a href="tel:02214710750"><FaPhone/> 0221 471-0750</a></li>
+                                            <li><a href="https://wa.me/2215448911"><IoLogoWhatsapp/> 221-544-8911</a></li>
+                                            <li><a href="https://wa.me/2215369997"><IoLogoWhatsapp/> 221-536-9997</a></li>
+                                            <li><a href="https://www.instagram.com/extintores_norte/"><BsInstagram/> Extintores_norte</a></li>
+                                            <li><a href="https://www.facebook.com/extintoresnorte/?locale=es_LA"><FaFacebook/> Extintores Norte</a></li>
+                                        </ul>
+                                    </div>  
+                                </Col>
+                                <Col xs={12} sm={12} md={7} lg={7} xl={5} className="h-100">
+                                    <Row className="bg-info">
+                                            <Col xs={12} sm={12} md={8} lg={11} xl={8} className="bg-danger">
+                                                <div className="d-flex flex-column justify-content-around align-items-center mt-5">
+                                                    <h3 className="fw-bold mb-3 mt-5 text-center text-white">HORARIOS</h3>
+                                                    <ul className="ul-horario">
+                                                        <li>LUNES a VIERNES</li>
+                                                        <li><FaClock /> 8.30hs a 17hs</li>
+                                                        <li>SABADOS</li>
+                                                        <li><FaClock />8.30hs a 13hs</li>
+                                                    </ul>
+                                                </div>
+                                            </Col>
+                                            <Col xs={12} sm={12} md={4} lg={1} xl={3} className="col-extintor-reloj bg-dark">
+                                                <img src="/img/animacion/matafuego-reloj.png" className="img-extintor"/>
+                                            </Col>
+                                    </Row>
+                                </Col>
+                        </Row>
+                </Container-fluid>
+            </section>
+
+            {/* ----------- Seccion video ----------- */}
 
             <section className="mt-5 pb-5">
                     <Container-fluid> 
@@ -113,10 +133,16 @@ export default function Home() {
                             </Col>
                         </Row>
                     </Container-fluid> 
-                </section>   
-                <a href="https://wa.me/2215369997" >
-                    <img src="/img/whats2.png" className="whatsapp"/>
-                </a>
+                </section>  
+
+            {/* ----------- Seccion whatsapp ----------- */}
+
+            <section className="mty-5">
+                    <a href="https://wa.me/2215369997" >
+                        <img src="/img/whats2.png" className="whatsapp"/>
+                    </a>
+            </section>   
+                
         </div>
     )
 }
