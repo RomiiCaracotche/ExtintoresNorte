e2137bbc1bbf63600828dbd82927e6d63b05c3d7
Romii <rocaracotche@gmail.com>
2025-11-29 18:06:16 -0300

    cambio diseño de titulos y adicion de flotas

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,8 +27,9 @@ export default function Home() {
             </div>
 
             {/* ----------- Seccion home ----------- */}
-            <section className="section-home">
-                <Container className="row-center-center">
+            <section className="section-home">  
+                <Container className="column-center-center p-0">
+                    <h3 className="titulo titulo-home">SOLUCIONES INTEGRALES</h3>
                     <Row className="row-home" >
                         {containerHome.map((item) => (
                             <Col xs={12} sm={12} md={6} lg={6} xl={3} className="col-home">
@@ -44,12 +45,85 @@ export default function Home() {
                 </Container>
             </section>
 
+            {/* ----------- Seccion flota ----------- */}
+            <section className="section-flota"> 
+                <Container className="column-center-center">
+                    <h3 className="titulo titulo-flota">NUESTRA FLOTA</h3>
+                    <Row className="row-flota">
+                        <Col xs={12} sm={12} md={12} lg={6} xl={6} className="col-flota">
+                            <img src="/img/home/camioneta-roja2.png"/>
+                        </Col>
+                        <Col xs={12} sm={12} md={12} lg={6} xl={6} className="col-flota">
+                            <img src="/img/servicios/camioneta-frente.png"/>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>      
+
+
+            {/* ----------- Seccion contacto/horario ----------- */}
+            <section className="section-contacto_horario">   
+                <Container>
+                        <Row className="row-contacto_horario">               
+                                <Col xs={12} sm={12} md={10} lg={8} xl={5} className="d-flex flex-column">
+                                    <h3 className="titulo titulo-contacto">CONTACTANOS</h3>
+                                    <div className="col-contacto_horario">
+                                        <ul className="ul-contacto">
+                                            <li><a href="mailto:extintoresnorte@hotmail.com" className="color-red correo"><MdAlternateEmail/> extintoresnorte@hotmail.com <MdOutlineAdsClick className="color-light"/></a></li>
+                                            <li><a href="tel:02214710750" className="color-light"><FaPhone/> 0221 471-0750 <MdOutlineAdsClick className="color-light"/></a></li>
+                                            <li><a href="https://wa.me/2215448911" className="color-green"><IoLogoWhatsapp/> 221-544-8911 <MdOutlineAdsClick className="color-light"/></a></li>
+                                            <li><a href="https://wa.me/2215369997" className="color-green"><IoLogoWhatsapp/> 221-536-9997 <MdOutlineAdsClick className="color-light"/></a></li>
+                                            <li><a href="https://www.instagram.com/extintores_norte/" className="color-blue"><BsInstagram/> Extintores_norte <MdOutlineAdsClick className="color-light"/></a></li>
+                                            <li><a href="https://www.facebook.com/extintoresnorte/?locale=es_LA" className="color-blue"><FaFacebook/> Extintores Norte <MdOutlineAdsClick className="color-light"/></a></li>
+                                        </ul>
+                                    </div>   
+                                </Col>
+                                <Col xs={12} sm={12} md={10} lg={8} xl={5} className="d-flex flex-column">
+                                        <h3 className="titulo titulo-horario">HORARIOS</h3>
+                                        <div className="col-contacto_horario">
+                                            <ul className="ul-horario">
+                                                <li className="color-red">LUNES a VIERNES</li>
+                                                <li><p className="hora color-light"><FaClock/> 8.30hs  a  17hs</p></li>
+                                                <li className="color-red">SABADOS</li>
+                                                <li><p className="hora color-light"><FaClock/> 8.30hs  a  13hs</p></li>
+                                            </ul>
+                                        </div>
+                                </Col> 
+                        </Row>
+                </Container>
+            </section>
+
+
+             {/* ----------- Seccion ubicacion ----------- */}
+            <section className="section-ubicacion">
+                <Container>
+                        <h3 className="titulo titulo-ubicacion">PODES ENCONTRARNOS EN</h3>
+                        <Row className="row-ubicacion">   
+                            <Col xs={12} md={10} lg={5} xl={6} xxl={5} className="col-ubicacion">   
+                                <div>
+                                    <ul className="ul-ubicacion">
+                                        <li>LA PLATA,</li>
+                                        <li><FaMapLocationDot className="color-light"/> Calle 519 N° 1820 esq. 13</li>
+                                    </ul>
+                                    <div className="col-ubicacion_img"> 
+                                        <img src="/img/animacion/matafuego-mapa2.png" />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={10} lg={5} xl={6} xxl={5} className="col-ubicacion">
+                                <Mapa />
+                            </Col>
+                        </Row>      
+                </Container>
+            </section>
+
 
             {/* ----------- Seccion video ----------- */}
             <section className="section-video">
-                <Container className="row-center-center">
+                <Container className="column-center-center">
+                    <h3 className="titulo titulo-video">¿ SABIAS ESTO ?</h3>
                     <Row className="row-video">
-                        <Col xs={12} md={12} xl={6} className="p-0">
+                        <Col xs={12} md={12} lg={12} xl={6} xxl={6} className="p-0">
                             <div className="container-video-info">
                                 <p>
                                     "En <span>Extintores Norte</span>, nuestro compromiso principal es tu seguridad. 
@@ -61,73 +135,12 @@ export default function Home() {
                                 </p>
                             </div>
                         </Col>
-                        <Col xs={12} md={11} xl={6} className="container-video-video">
+                        <Col xs={12} md={12} lg={11} xl={6} xxl={6} className="container-video-video">
                                 <video src="/video/video.mp4" controls/>
                         </Col>
                     </Row>
                 </Container>
             </section> 
-
-
-            {/* ----------- Seccion contacto/horario ----------- */}
-            <section className="section-contacto_horario">   
-                <Container>
-                        <Row className="row-contacto_horario">            
-                                <Col xs={12} sm={12} md={10} lg={8} xl={5} className="col-contacto_horario">
-                                    <div className="container-contacto-info">
-                                        <h3 className="titulo">CONTACTANOS</h3>
-                                        <ul className="ul-contacto">
-                                            <li><a href="mailto:extintoresnorte@hotmail.com" className="color-red correo"><MdAlternateEmail/> extintoresnorte@hotmail.com <MdOutlineAdsClick className="color-light"/></a></li>
-                                            <li><a href="tel:02214710750" className="color-light"><FaPhone/> 0221 471-0750 <MdOutlineAdsClick className="color-light"/></a></li>
-                                            <li><a href="https://wa.me/2215448911" className="color-green"><IoLogoWhatsapp/> 221-544-8911 <MdOutlineAdsClick className="color-light"/></a></li>
-                                            <li><a href="https://wa.me/2215369997" className="color-green"><IoLogoWhatsapp/> 221-536-9997 <MdOutlineAdsClick className="color-light"/></a></li>
-                                            <li><a href="https://www.instagram.com/extintores_norte/" className="color-blue"><BsInstagram/> Extintores_norte <MdOutlineAdsClick className="color-light"/></a></li>
-                                            <li><a href="https://www.facebook.com/extintoresnorte/?locale=es_LA" className="color-blue"><FaFacebook/> Extintores Norte <MdOutlineAdsClick className="color-light"/></a></li>
-                                        </ul>
-                                    </div>   
-                                </Col>
-                                
-                               <Col xs={12} sm={12} md={10} lg={8} xl={5} className="col-contacto_horario col-horario">
-                                        <div className="container-horario-info">
-                                            <div className="container-horario_horario">
-                                                <h3 className="titulo">HORARIOS</h3>
-                                                <ul className="ul-horario">
-                                                    <li className="color-red">LUNES a VIERNES</li>
-                                                    <li><p className="hora color-light"><FaClock/> 8.30hs  a  17hs</p></li>
-                                                    <li className="color-red">SABADOS</li>
-                                                    <li><p className="hora color-light"><FaClock/> 8.30hs  a  13hs</p></li>
-                                                </ul>
-                                            </div>
-                                            <div className="container-horario_img">
-                                                <img src="/img/animacion/matafuego-reloj.png"/>
-                                            </div>
-                                        </div>
-                                </Col> 
-                        </Row>
-                </Container>
-            </section>
-
-
-             {/* ----------- Seccion ubicacion ----------- */}
-            <section className="section-ubicacion">
-                <Container className="row-center-center">
-                        <Row className="row-ubicacion">   
-                            <Col xs={12} md={10} lg={10} className="col-ubicacion"> 
-                                <h3 className="titulo">PODES ENCONTRARNOS EN</h3>
-                                <ul className="ul-ubicacion">
-                                    <li>LA PLATA,</li>
-                                    <li><FaMapLocationDot className="color-light"/> Calle 519 N° 1820 esq. 13</li>
-                                </ul>
-                            </Col>
-                            <Col xs={12} lg={7} xl={6}className="col-ubicacion_mapa">
-                                <Mapa />
-                            </Col>
-                            <Col xs={12} lg={3} xl={3} className="col-ubicacion_img">
-                                <img src="/img/animacion/matafuego-mapa2.png" />
-                            </Col>
-                        </Row>
-                </Container>
-            </section>
             
  
             {/* ----------- Seccion whatsapp ----------- */}
